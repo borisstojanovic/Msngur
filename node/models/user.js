3e7a5e5f4deebb7aff5e2a5c0a44c60b930442de
Boris Stojanovic <bstojanovic3518rn@raf.rs>
2021-07-01 14:05:01 +0200

    Renamed some of the files

---
+++ node/models/user.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    communications: {
+        type: Array,
+        required: false
+    }
+}, {
+    timestamps: true
+})
+
+
+UserSchema.pre('save', function(next) {
+    let user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        else{
+            cb(null, isMatch);
+        }
+    });
+};
+
+module.exports = mongoose.model("User", UserSchema);
